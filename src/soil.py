a532fbe66c97c0cb0fa89797466e9ff838fe388f
tylen101 <tylenwcarroll@gmail.com>
2024-01-07 15:50:13 -0500

    began watering

--- src/soil.py
+++ src/soil.py
@@ -1,7 +1,7 @@
-from mimetypes import init
 import pygame
 from settings import *
 from os import path
+from pytmx.util_pygame import load_pygame
 
 
 class SoilLayer:
@@ -12,16 +12,20 @@ class SoilLayer:
     self.soil_sprites = pygame.sprite.Group()
 
     # graphics
-    soil_path = path('graphics', 'soil', 'o.png')
+    soil_path = path.join('graphics', 'soil', 'o.png')
     self.soil_surf = pygame.image.load(soil_path)
 
     self.create_soil_grid()
 
     # requirements
   def create_soil_grid(self):
-    # if area is farmable
-    ground_path = path('graphics', 'world', 'ground.png')
-    ground = pygame.image.load(ground_path)
-    h_tiles = ground.get_width() // TILE_SIZE, ground.get_height() // TILE_SIZE
-    # if area has been watered
 
+    # if area is farmable
+    ground_path = path.join('graphics', 'world', 'ground.png')
+    ground = pygame.image.load(ground_path)
+    h_tiles, v_tiles = ground.get_width() // TILE_SIZE, ground.get_height() // TILE_SIZE
+
+    self.grid = [[[] for col in range(h_tiles)] for row in range(v_tiles)]
+    tmx_path = path.join('data', 'map.tmx')
+    for x, y, _ in load_pygame(tmx_path).get_layer_by_name('Farmable').tiles():
+      self.grid[y][x].append('F')
